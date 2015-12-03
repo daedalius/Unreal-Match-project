3e4a163faf6045fee5ff47edfc5556628ee71d0f
Dmitriy_Tishin <daedalius@mail.ru>
2015-12-03 14:24:30 +0300

    Refactored "GetRectangle" function in CameraBahaviour (used Clamp)

--- Unreal-Match/Unreal-Match/Resources/Scripts/Game/HUD/Camera/CameraBahaviour.ts
+++ Unreal-Match/Unreal-Match/Resources/Scripts/Game/HUD/Camera/CameraBahaviour.ts
@@ -1,6 +1,8 @@
 ﻿import GameObject = require('../../Entities/Objects/GameObject');
 import UnrealMatch = require('../../UnrealMatch');
 
+import Algorithms = require('../../Algorithms');
+
 import Point = require('../../Entities/Primitives/Point.Primitive');
 import Vector = require('../../Entities/Primitives/Vector.Primitive');
 import Rectangle = require('../../Entities/Primitives/Rectangle.Primitive');
@@ -8,48 +10,12 @@ import Rectangle = require('../../Entities/Primitives/Rectangle.Primitive');
 abstract class CameraBahaviour {
     /** Returns correct area to drow as rectangle in OCS */
     public GetRectangle(): Rectangle {
-        var startX: number;
-        var startY: number;
         var camera: Point = this.GetPoint();
 
         // find x part
-        var halfOfFrameWidth = UnrealMatch.ORIGIN.Width / 2;
-
-        if (camera.X < halfOfFrameWidth) {
-            // close to left
-            startX = halfOfFrameWidth;
-        }
-        else
-        {
-            if (camera.X > UnrealMatch.LEVEL.Width - halfOfFrameWidth) {
-                // close to right
-                startX = UnrealMatch.LEVEL.Width - halfOfFrameWidth;
-            }
-            else {
-                // no need any correction
-                // TODO: refactor for better CPU prediction
-                startX = camera.X;
-            }
-        }
-
+        var startX: number = Algorithms.Clamp(camera.X, UnrealMatch.ORIGIN.Width / 2, UnrealMatch.LEVEL.Width - UnrealMatch.ORIGIN.Width / 2);
         // find y part
-        var halfOfFrameHeight = UnrealMatch.ORIGIN.Height / 2;
-
-        if (camera.Y < halfOfFrameHeight) {
-            // close to bottom
-            startY = halfOfFrameHeight;
-        }
-        else {
-            if (camera.Y > UnrealMatch.LEVEL.Height - halfOfFrameHeight) {
-                // close to top
-                startY = UnrealMatch.LEVEL.Height - halfOfFrameHeight;
-            }
-            else {
-                // no need any correction
-                // TODO: refactor for better CPU prediction
-                startY = camera.Y;
-            }
-        }
+        var startY: number = Algorithms.Clamp(camera.Y, UnrealMatch.ORIGIN.Height / 2, UnrealMatch.LEVEL.Height - UnrealMatch.ORIGIN.Height / 2);
 
         return new Rectangle(new Point(startX, startY), new Point(startX + UnrealMatch.ORIGIN.Width, startY + UnrealMatch.ORIGIN.Height));
     }
